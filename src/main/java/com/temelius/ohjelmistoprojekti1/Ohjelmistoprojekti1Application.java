773ff6b1e0089f16dc31e86f133119d954aa618d
piebob9x <55234449+piebob9x@users.noreply.github.com>
2021-04-16 13:07:43 +0300

    Added postgres credentials

--- src/main/java/com/temelius/ohjelmistoprojekti1/Ohjelmistoprojekti1Application.java
+++ src/main/java/com/temelius/ohjelmistoprojekti1/Ohjelmistoprojekti1Application.java
@@ -35,6 +35,12 @@ public class Ohjelmistoprojekti1Application {
 			UserRepository urepository) {
 		return (args) -> {
 			
+			urepository.deleteAll();
+			qrepository.deleteAll();
+			quizRepository.deleteAll();
+			uarepository.deleteAll();
+			arepository.deleteAll();
+			
 			urepository.save(new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", "user@quizapp.fi"));
 			urepository.save(new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN", "admin@quizapp.fi"));
 			
