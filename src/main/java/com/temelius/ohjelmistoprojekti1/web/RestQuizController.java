ae591fa627d38fd7569f938b27c6ba6a8150fee2
joonasrom <jpr97@luukku.com>
2021-04-28 23:49:24 +0300

    Fixing CORS for post

--- src/main/java/com/temelius/ohjelmistoprojekti1/web/RestQuizController.java
+++ src/main/java/com/temelius/ohjelmistoprojekti1/web/RestQuizController.java
@@ -35,7 +35,7 @@ import com.temelius.ohjelmistoprojekti1.model.QuizRepository;
 import com.temelius.ohjelmistoprojekti1.model.UserAnswer;
 import com.temelius.ohjelmistoprojekti1.model.UserAnswerRepository;
 
-@CrossOrigin(origins = "http://localhost:3000/")
+@CrossOrigin
 @RestController
 @RequestMapping("api")
 public class RestQuizController {
@@ -131,7 +131,7 @@ public class RestQuizController {
 	
 	
 
-	@CrossOrigin(origins = "http://localhost:3000")
+	@CrossOrigin (origins = "http://localhost:3000/")
 		@PostMapping(value="/useranswers")
 		UserAnswer newUserAnswer(@RequestBody UserAnswer newUserAnswer) {
 		    return uarepository.save(newUserAnswer);
